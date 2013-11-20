373057b5b572dbbd4a9616aeb8766f1991647413
Leonardo Santagada <santagada@gmail.com>
2013-11-20 12:50:24 -0200

    smooth scroll

--- js/main.js
+++ js/main.js
@@ -1,3 +1,7 @@
 $(function () {
     $('.container').fitVids();
-})
+    $('.navbar').bind('click', 'ul li a', function(event) {
+        $.scrollTo(event.target.hash, 250);
+        event.preventDefault();
+    });
+});
